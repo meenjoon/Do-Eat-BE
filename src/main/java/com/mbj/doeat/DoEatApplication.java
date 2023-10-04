c60bb377a8131355dfc22f8074fcc6ee97124590
meenjoon <zqudwnsz@naver.com>
2023-10-04 12:40:28 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/mbj/doeat/DoEatApplication.java
@@ -0,0 +1,13 @@
+package com.mbj.doeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoEatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mbj/doeat/DoEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbj.doeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoEatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
