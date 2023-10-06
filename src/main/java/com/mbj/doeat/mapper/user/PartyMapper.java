082e2a0a5208b3724095ab6adfece3856e81ee7c
meenjoon <zqudwnsz@naver.com>
2023-10-07 00:28:21 +0900

    feat: PartyMapper 구현

---
+++ src/main/java/com/mbj/doeat/mapper/user/PartyMapper.java
@@ -0,0 +1,31 @@
+package com.mbj.doeat.mapper.user;
+
+import com.mbj.doeat.dto.party.PartyCreateRequestDto;
+import com.mbj.doeat.dto.party.PartyCreateResponseDto;
+import com.mbj.doeat.entity.Party;
+
+public class PartyMapper {
+
+    public static Party mapToEntity(PartyCreateRequestDto requestDto) {
+        Party party = new Party();
+        party.setUserId(requestDto.getUserId());
+        party.setRestaurantName(requestDto.getRestaurantName());
+        party.setRestaurantLocation(requestDto.getRestaurantLocation());
+        party.setRecruitmentLimit(requestDto.getRecruitmentLimit());
+        if (requestDto.getDetail() != null) {
+            party.setDetail(requestDto.getDetail());
+        }
+        return party;
+    }
+
+    public static PartyCreateResponseDto mapToResponseDto(Party party) {
+        PartyCreateResponseDto responseDto = new PartyCreateResponseDto();
+        responseDto.setPostId(party.getPostId());
+        responseDto.setUserId(party.getUserId());
+        responseDto.setRestaurantName(party.getRestaurantName());
+        responseDto.setRestaurantLocation(party.getRestaurantLocation());
+        responseDto.setRecruitmentLimit(party.getRecruitmentLimit());
+        responseDto.setDetail(party.getDetail() != null ? party.getDetail() : "");
+        return responseDto;
+    }
+}
