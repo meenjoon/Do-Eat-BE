0f19a95e0a460844f97c5009cdd42e4f9b65de66
meenjoon <zqudwnsz@naver.com>
2023-10-07 00:26:26 +0900

    feat: PartyRepository 구현

---
+++ src/main/java/com/mbj/doeat/repository/PartyRepository.java
@@ -0,0 +1,8 @@
+package com.mbj.doeat.repository;
+
+import com.mbj.doeat.entity.Party;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartyRepository extends JpaRepository<Party, Long> {
+
+}
