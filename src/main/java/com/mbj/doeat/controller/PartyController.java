1e58790bad9fc54f7ac211833b8f623514ad226f
meenjoon <zqudwnsz@naver.com>
2023-10-07 15:35:35 +0900

    feat: 모든 파티를 가져오는 getAllParties() 컨트롤러 구현

--- src/main/java/com/mbj/doeat/controller/PartyController.java
+++ src/main/java/com/mbj/doeat/controller/PartyController.java
@@ -6,10 +6,8 @@ import com.mbj.doeat.service.party.PartyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +21,17 @@ public class PartyController {
         try {
             PartyCreateResponseDto responseDto = partyService.createParty(requestDto);
             return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+        } catch (Exception e) {
+            String errorMessage = "오류가 발생했습니다: " + e.getMessage();
+            return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllParties() {
+        try {
+            List<PartyCreateResponseDto> parties = partyService.getAllParties();
+            return new ResponseEntity<>(parties, HttpStatus.OK);
         } catch (Exception e) {
             String errorMessage = "오류가 발생했습니다: " + e.getMessage();
             return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
