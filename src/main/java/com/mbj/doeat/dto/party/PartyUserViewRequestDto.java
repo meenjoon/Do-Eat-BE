f128b65425efc083b63e3942797f888a94c2129b
meenjoon <zqudwnsz@naver.com>
2023-10-07 17:54:20 +0900

    feat: PartyUserViewRequestDto 생성

---
+++ src/main/java/com/mbj/doeat/dto/party/PartyUserViewRequestDto.java
@@ -0,0 +1,15 @@
+package com.mbj.doeat.dto.party;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PartyUserViewRequestDto {
+
+    private Long userId;
+}
